1985381b8844529ffc94d90f0fbbe6fbdcb25198
Antonio Duarte <durdy48@gmail.com>
2018-01-03 11:31:08 +0100

    Chapter 9 - Pattern Matching

---
+++ 9-PatternMatching/Extractor.scala
@@ -0,0 +1,38 @@
+object StockService {
+  def process(input: String) {
+    input match {
+      case Symbol() => println(s"Look up price for valid symbol $input")
+      case ReceiveStockPrice(symbol @ Symbol(), price) =>
+        println(s"Received price $$$price for symbol $symbol")
+      case _ => println(s"Invalid input $input")
+    }
+  }
+}
+
+object ReceiveStockPrice {
+  def unapply(input: String) : Option[(String, Double)] = {
+    try {
+      if ( input contains ":") {
+        val splitQuote = input split ":"
+        Some(splitQuote(0), splitQuote(1).toDouble)
+      }
+      else {
+        None
+      }
+    } catch {
+      case _ : NumberFormatException => None
+    }
+  }
+}
+
+object Symbol {
+  def unapply(symbol: String) : Boolean = {
+    // you'd look up a database... here only GOOG and IBM are recognized
+    symbol == "GOOG" || symbol == "IBM"
+  }
+}
+
+StockService process "GOOG"
+StockService process "GOOG:310.84"
+StockService process "GOOG:BUY"
+StockService process "ERR:12.21"
---
+++ 9-PatternMatching/MatchWithField.scala
@@ -0,0 +1,18 @@
+class Sample {
+  val max = 100
+
+  def process(input: Int) {
+    input match {
+      case max => println(s"You matched max $max")
+    }
+  }
+}
+
+val sample = new Sample
+try {
+  sample.process(0)
+} catch {
+  case ex: Throwable => println(ex)
+}
+sample.process(100)
+

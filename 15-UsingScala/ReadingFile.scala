a853a7a932e7d6ca02ae32be13dbe5a9a223f61e
Antonio Duarte <durdy48@gmail.com>
2018-01-07 19:03:07 +0100

    Chapter 15- Using Scala

---
+++ 15-UsingScala/ReadingFile.scala
@@ -0,0 +1,4 @@
+import scala.io.Source
+
+println("*** The content of the file you read is:")
+Source.fromFile("ReadingFile.scala").foreach { print }
---
+++ 15-UsingScala/WriteToFile.scala
@@ -0,0 +1,5 @@
+import java.io._
+
+val writer = new PrintWriter(new File("symbols.txt"))
+writer write "AAPL"
+writer.close()
